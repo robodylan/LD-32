d58da575fd7ac0944ea0938d4f5f095166601221
Dylan Dunn <robodylan123@gmail.com>
2015-04-18 11:46:00 -0400

    Added Enemy and Health

---
+++ LD32/LD32/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using Dunn_2D;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD32
+{
+    class Enemy : Entity
+    {
+        public Enemy(string filename, int x, int y, bool hasPhysics)
+            : base(filename, x, y, hasPhysics)
+        {
+        }
+
+        public override void Move(float x, float y)
+        {
+
+        }
+    }
+}
--- LD32/LD32/GameInstance.cs
+++ LD32/LD32/GameInstance.cs
@@ -11,36 +11,50 @@ namespace LD32
     {
         public static Entity player;
         public static List<Block> map;
+        public static List<Entity> enemies;
         public override void Setup()
         {
-            this.setScale(30);
-            player = new Entity("content/player.png", 0, 0, true);
-            map = Util.getBlocksFrom("content/map.png", "content/player.png", 0);
-            this.AddToScene(player);
+            this.setTitle("LD 32");
+            this.setScale(64);
+            player = new Entity("content/img/player", 0, 0, true);
+            map = Util.getBlocksFrom("content/lvl/map.png", "content/img/Floor01.png", 0);
+            enemies = Util.getEntitiesFrom("content/lvl/map.png", "content/img/enemy", 255);
+            this.entities = enemies;
             this.blocks = map;
-            this.setFPS(120);
-            this.setGravity(.01f);
+            this.AddToScene(player);
+            this.setFPS(60);
+            this.setGravity(.25f);
+            player.Health = 100;
+            player.killedByTouch = true;
         }
 
         public override void Update()
         {
             handleInput();
             setCenter((int)player.getX(), (int)player.getY());
+            foreach(Block b in blocks)
+            {
+                b.setHoverColor(255,0,0);
+            }
+            if(player.touching)
+            {
+                player.Health--;
+            }
         }
 
         public void handleInput()
         {
             if (Input.isKeyDown(Input.Key.Jump) && player.isColliding)
             {
-                player.setVelocity(player.getVelocityX(), -1f);
+                player.setVelocity(player.getVelocityX(), -7.5f);
             }
             if (Input.isKeyDown(Input.Key.Left))
             {
-                player.setVelocity(-1f, player.getVelocityY());
+                player.setVelocity(-4f, player.getVelocityY());
             }
             if (Input.isKeyDown(Input.Key.Right))
             {
-                player.setVelocity(1f, player.getVelocityY());
+                player.setVelocity(4f , player.getVelocityY());
             }
         }
     }
