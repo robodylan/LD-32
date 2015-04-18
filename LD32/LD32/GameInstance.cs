9ca362d8a8b8ffecb54cb07d3b97cf1b213d18af
Dylan Dunn <robodylan123@gmail.com>
2015-04-17 23:01:25 -0400

    Added input, map loading, and physics

--- LD32/LD32/GameInstance.cs
+++ LD32/LD32/GameInstance.cs
@@ -10,19 +10,38 @@ namespace LD32
     class GameInstance : Game 
     {
         public static Entity player;
-        public static Block block;
+        public static List<Block> map;
         public override void Setup()
         {
+            this.setScale(30);
             player = new Entity("content/player.png", 0, 0, true);
-            block = new Block("content/player.png", 0, 100, true);
+            map = Util.getBlocksFrom("content/map.png", "content/player.png", 0);
             this.AddToScene(player);
-            this.AddToScene(block);
-            this.setFPS(60);
+            this.blocks = map;
+            this.setFPS(120);
+            this.setGravity(.01f);
         }
 
         public override void Update()
         {
-            player.addVelocity(0,0.01f);
+            handleInput();
+            setCenter((int)player.getX(), (int)player.getY());
+        }
+
+        public void handleInput()
+        {
+            if (Input.isKeyDown(Input.Key.Jump) && player.isColliding)
+            {
+                player.setVelocity(player.getVelocityX(), -1f);
+            }
+            if (Input.isKeyDown(Input.Key.Left))
+            {
+                player.setVelocity(-1f, player.getVelocityY());
+            }
+            if (Input.isKeyDown(Input.Key.Right))
+            {
+                player.setVelocity(1f, player.getVelocityY());
+            }
         }
     }
 }
